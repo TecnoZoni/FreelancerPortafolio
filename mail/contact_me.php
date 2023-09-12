b76d2a3786d4596eda6cc5bfdeac10256e40e0ea
Agustin Minzoni <agustinminzoni@gmail.com>
2023-09-12 15:19:42 -0300

    acomodos de ultimo momento

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,7 +18,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'agustinminzoni@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Corro del sitio web por:  $name";
-$email_body = "Rcibiste un nuevo mensaje desde el formulario de tu pagina web.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: $phone\n\nMensaje:\n$message";
+$email_body = "Recibiste un nuevo mensaje desde el formulario de tu pagina web.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: $phone\n\nMensaje:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
